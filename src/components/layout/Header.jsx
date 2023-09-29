9a6d1261f86a0f5b98dd88d18be29771e681f520
Jose Antonio Vargas Garcia <tonacovargas@hotmail.com>
2023-09-29 02:54:38 +0200

    Add files via upload

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -5,7 +5,7 @@ export const Header = () => {
     <header className="header">
             <div /* className="logo" */ className='camara'>
                 {/* <div className="play"></div> */}
-                <img  src="public/img/62843filmprojector_109301.png" alt="camara" />
+                <img  src="img/62843filmprojector_109301.png" alt="camara" />
             </div>            
             <h1>Mi_Blog_de_Cine</h1>
         </header>
