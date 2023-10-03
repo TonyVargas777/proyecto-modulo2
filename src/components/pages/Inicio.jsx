bd182a15092793fd02f232b60046951d2348e759
Jose Antonio Vargas Garcia <tonacovargas@hotmail.com>
2023-10-03 14:34:26 +0200

    commit imagen inicio

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -5,7 +5,8 @@ export const Inicio = () => {
   return (
     <div className='jumbo'>
       <h1 className='type'>Bienvenido a Mi_Blog</h1>
-      <h3>Blog desarrollado con el MERN Stack</h3>
+      <h2>Blog desarrollado con el MERN Stack</h2>
+      <img src="./public/img/mern7.jpeg" id='mern' alt="" />
       <h3>(Mongo, Express, React y NodeJS)</h3>
       <br/>
       <Link to="/articulos" className='button'>Ver los artículos</Link>
