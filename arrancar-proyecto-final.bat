d477db30cb87db8b0db7b11b67260f7aedb880a6
Jose Antonio Vargas Garcia <tonacovargas@hotmail.com>
2023-10-10 14:50:42 +0200

    Delete arrancar-proyecto-final.bat

--- arrancar-proyecto-final.bat
+++
@@ -1,3 +0,0 @@
-start cmd.exe /k "cd C:\Program Files\MongoDB\Server\7.0\bin && mongod.exe --dbpath C:\data7\db"
-start cmd.exe /k "cd C:\Users\Raquel\Desktop\UDEMY\REACT\api-rest-node && npm start"
-start cmd.exe /k "cd C:\Users\Raquel\Desktop\UDEMY\REACT\Proyecto_Final && npm run dev"
