b5be4a8ce3fd45fb0e46f8d8959d02b886f82637
Jose Antonio Vargas Garcia <tonacovargas@hotmail.com>
2023-10-13 03:39:54 +0200

    Add files via upload

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -7,8 +7,8 @@ export const Inicio = () => {
       <h1 className='type'>Bienvenido a Mi_Blog</h1>
       <h2>Blog desarrollado con el MERN Stack</h2>
       <img src="/img/mern7.jpeg" id='mern' alt=""/>
-      <h3>(Mongo, Express, React y NodeJS)</h3>
-      <br/>
+      <h3>(Mongo, Express, React y NodeJS)</h3>      
+      <h3>(FULLY RESPONSIVE)</h3>     
       <Link to="/articulos" className='button'>Ver los artículos</Link>
     </div>
   )
