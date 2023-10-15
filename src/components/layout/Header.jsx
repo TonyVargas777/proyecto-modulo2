eab6f237c3c2edf0804a44565fd35eb96645e272
Jose Antonio Vargas Garcia <tonacovargas@hotmail.com>
2023-10-15 04:40:43 +0200

    commit header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -4,7 +4,7 @@ export const Header = () => {
   return (
     <header className="header">
             <div className='camara'>                
-                <img  src="img/cinema.gif" alt="camara" />
+                <img  src="img/62843filmprojector_109301.png" alt="camara" />
             </div>            
             <h1>Mi_Blog_de_Cine</h1>
         </header>
