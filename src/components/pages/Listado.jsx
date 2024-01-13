18dccf867fe9ce9a35b6bd69e80d4584d5c7ee6d
Jose Antonio Vargas Garcia <tonacovargas@hotmail.com>
2024-01-13 20:38:44 +0100

    Add files via upload

--- src/components/pages/Listado.jsx
+++ src/components/pages/Listado.jsx
@@ -15,19 +15,14 @@ export const Listado = ({articulos, setArticulos}) => {
     articulos.map((articulo) => {
         return (
           <article key={articulo._id} className="articulo-item">
+            <Link to={"/articulo/"+articulo._id} className="mascara">
             <div className="mascara">
               {articulo.imagen != "default.png" && <img src={Global.url+"imagen/"+articulo.imagen} alt="poster"/>}
               {articulo.imagen == "default.png" && <img src="https://www.idsplus.net/wp-content/uploads/js-logo-badge-512.png"/>}
-            </div>         
+            </div>  
+            </Link>       
             <div className="datos">
-              <h3 className="title"><Link to={"/articulo/"+articulo._id}>{articulo.titulo}</Link></h3>
-              {/* <p className="descripcion"><b>{articulo.fecha}</b></p>
-              <p className="descripcion">{articulo.contenido}</p> */}
-              
-              {/* <Link to={"/editar/"+articulo._id} className="edit">Editar</Link>
-              <button className="delete" onClick={()=>{
-                eliminar(articulo._id)
-              }}>Borrar</button> */}
+              <h3 className="title"><Link to={"/articulo/"+articulo._id}>{articulo.titulo}</Link></h3>              
             </div>
           </article>
         );
